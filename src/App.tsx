d54a51826a90e24fe45bb1fc7641822db018e63c
Kristuwa <121851618+KrisKrasovska@users.noreply.github.com>
2024-07-10 08:20:53 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,46 @@
-import React, { FC } from "react";
-import "./App.css";
-import { theme } from "./utils/theme";
-import { ThemeProvider } from "styled-components";
-import { GlobalStyle } from "./GlobalStyle";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import { Layout } from "./components/Layout/Layout";
-import NotFound from "./pages/NotFound";
-import { ToastContainer } from "react-toastify";
-import Facts from "./pages/Facts";
-import Recipes from "./pages/Recipes";
-import Combinations from "./pages/Combinations";
-import History from "./pages/History";
-import BeansPage from "./pages/BeansPage";
-import RecipesPage from "./pages/RecipesPage";
+import React, { FC } from 'react'
+import './App.css'
+import { theme } from './utils/theme'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './GlobalStyle'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import { Layout } from './components/Layout/Layout'
+import NotFound from './pages/NotFound'
+import { ToastContainer } from 'react-toastify'
+import Facts from './pages/Facts'
+import Recipes from './pages/Recipes'
+import Combinations from './pages/Combinations'
+import History from './pages/History'
+import BeansPage from './pages/BeansPage'
+import RecipesPage from './pages/RecipesPage'
 
 const router = createBrowserRouter(
   [
     {
-      path: "/",
+      path: '/',
       element: <Layout />,
       children: [
         { index: true, element: <HomePage /> },
-        { path: "beans/:id", element: <BeansPage /> },
-        { path: "facts", element: <Facts /> },
-        { path: "recipes", element: <Recipes /> },
-        { path: "recipes/:id", element: <RecipesPage /> },
-        { path: "combinations", element: <Combinations /> },
-        { path: "history", element: <History /> },
+        { path: 'beans/:id', element: <BeansPage /> },
+        { path: 'facts', element: <Facts /> },
+        { path: 'recipes', element: <Recipes /> },
+        { path: 'recipes/:id', element: <RecipesPage /> },
+        { path: 'combinations', element: <Combinations /> },
+        { path: 'history', element: <History /> },
       ],
     },
-    { path: "*", element: <NotFound /> },
+    { path: '*', element: <NotFound /> },
   ],
-  { basename: "/beans" }
-);
+  { basename: '/beans' }
+)
 
 const App: FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <ToastContainer
         icon={false}
-        position="top-right"
+        position='top-right'
         autoClose={5000}
         hideProgressBar={false}
         newestOnTop={false}
@@ -49,12 +49,12 @@ const App: FC = () => {
         pauseOnFocusLoss
         draggable
         pauseOnHover
-        theme="colored"
+        theme='colored'
       />
       <GlobalStyle />
       <RouterProvider router={router} />
     </ThemeProvider>
-  );
-};
+  )
+}
 
-export default App;
+export default App
